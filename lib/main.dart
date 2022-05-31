2c6627694298e24be1f8d79e25797aec3e3f59d9
areeshak <98297607+areeshak@users.noreply.github.com>
2022-05-31 22:31:10 +0500

    Add images & navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const ExplorePage(),
+      home: const ExploreScreen(),
     );
   }
 }
--- lib/views/explore.dart
+++ lib/views/explore.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/views/locationDetails.dart';
 import '../entities/locations.dart';
 
-class ExplorePage extends StatefulWidget {
-  const ExplorePage({Key? key}) : super(key: key);
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
 
   @override
-  State<ExplorePage> createState() => _ExplorePageState();
+  State<ExploreScreen> createState() => _ExploreScreenState();
 }
 
-class _ExplorePageState extends State<ExplorePage> {
+class _ExploreScreenState extends State<ExploreScreen> {
   List<Locations> locations = [
     Locations(
       name: 'Skardu',
       tag: 'Mountains',
-      imageLink: 'skardu-1.jpg',
+      imageLink: [
+        'skardu-1.jpg',
+        'skardu-2.jpg',
+        'skardu-3.jpg',
+      ],
       description:
-      "Skardu Baltistan lies in the heart of world's mightiest mountain ranges, the Great Himalaya and the Karakoram.",
+          "Skardu Baltistan lies in the heart of world's mightiest mountain ranges, the Great Himalaya and the Karakoram.",
       itinerary: [
         'Satpara Lake',
         'Lower Kachura Lake',
@@ -26,9 +31,13 @@ class _ExplorePageState extends State<ExplorePage> {
     Locations(
       name: 'Kumrat Valley',
       tag: 'Mountains',
-      imageLink: 'kumrat_valley-1.jpg',
+      imageLink: [
+        'kumrat_valley-1.jpg',
+        'kumrat_valley-2.jpg',
+        'kumrat_valley-3.jpg',
+      ],
       description:
-      "Kumrat Valley is a famous tourist destination located in the Upper Dir District of Khyber Pakhtunkhwa the Province of Pakistan.",
+          "Kumrat Valley is a famous tourist destination located in the Upper Dir District of Khyber Pakhtunkhwa the Province of Pakistan.",
       itinerary: [
         'Panjkora River',
         'Do Kala Chasma',
@@ -40,9 +49,13 @@ class _ExplorePageState extends State<ExplorePage> {
     Locations(
       name: 'Fairy Meadows',
       tag: 'Trek',
-      imageLink: 'fairy_meadows-1.jpg',
+      imageLink: [
+        'fairy_meadows-1.jpg',
+        'fairy_meadows-2.jpg',
+        'fairy_meadows-3.jpg',
+      ],
       description:
-      "Fairy Meadows is the heart of exotic North Pakistan, It is located at the base of Nanga Parbat.",
+          "Fairy Meadows is the heart of exotic North Pakistan, It is located at the base of Nanga Parbat.",
       itinerary: [
         'Jeep ride from Raikot Bridge to Tatu Village',
         'Hike for 3-4 hours towards the Meadows',
@@ -55,9 +68,13 @@ class _ExplorePageState extends State<ExplorePage> {
     Locations(
       name: 'Shogran',
       tag: 'Mountaines',
-      imageLink: 'shogran.jpg',
+      imageLink: [
+        'shogran-1.jpg',
+        'shogran-2.jpg',
+        'shogran-3.jpg',
+      ],
       description:
-      "Shogran is a green plateau in the popular tourist area of Kaghan Valley Pakistan.",
+          "Shogran is a green plateau in the popular tourist area of Kaghan Valley Pakistan.",
       itinerary: [
         'Makra Peak',
         'Siri Paye',
@@ -85,53 +102,62 @@ class _ExplorePageState extends State<ExplorePage> {
       ),
       body: ListView.builder(
         itemCount: locations.length,
-        itemBuilder: (context, index) => Container(
-          margin: const EdgeInsets.all(15),
-          height: MediaQuery.of(context).size.height * 0.2,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Positioned.fill(
-                child: ClipRRect(
-                  //rounded image borders
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.asset(
-                    'images/${locations[index].imageLink}',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Text(
-                locations[index].name,
-                style: const TextStyle(
-                  fontSize: 35,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.03,
-                  width: MediaQuery.of(context).size.width * 0.23,
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      locations[index].tag,
-                      style: const TextStyle(
-                        fontSize: 15,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
+        itemBuilder: (context, index) => InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                  builder: (context) =>
+                      LocationDetailsScreen(location: locations[index])),
+            );
+          },
+          child: Container(
+            margin: const EdgeInsets.all(15),
+            height: MediaQuery.of(context).size.height * 0.2,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned.fill(
+                  child: ClipRRect(
+                    //rounded image borders
+                    borderRadius: BorderRadius.circular(30),
+                    child: Image.asset(
+                      'images/${locations[index].imageLink[0]}',
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.blueGrey.withOpacity(0.8),
+                ),
+                Text(
+                  locations[index].name,
+                  style: const TextStyle(
+                    fontSize: 35,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.03,
+                    width: MediaQuery.of(context).size.width * 0.23,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        locations[index].tag,
+                        style: const TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blueGrey.withOpacity(0.8),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
